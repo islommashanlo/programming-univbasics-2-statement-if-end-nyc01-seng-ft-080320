0e74c3e939f14f96223d43927f3ffdf2931e3719
Islom Mashanlo <islom.mashanlo@gmail.com>
2020-07-22 19:31:35 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,8 +1,8 @@
 # Write your solution here
 def contrast
-if time.now % 2 == 0
-  puts "Even!"
-else
-  puts "Odd!"
-end
+  if time.now % 2 == 0
+    puts "Even!"
+  else
+    puts "Odd!"
+  end
 end
